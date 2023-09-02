f91c22cee076c070f879023d557416b9ea7164b3
Felix Edström <felix.edstrom1@gmail.com>
2023-09-02 11:10:27 +0200

    påväg med formuläret

--- src/pages/addThread/AddThread.tsx
+++ src/pages/addThread/AddThread.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import AddThreadForm from '../../components/AddThreadForm/AddThreadForm'
+import '../../components/AddThreadForm/addThreadForm.css'
+
 
 const AddThread = () => {
   return (
     <div className='wrapper'>
         <h3>Add new thread</h3>
         <p>Form for add new thread</p>
+        <AddThreadForm />
     </div>
   )
 }
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import './home.css'
 
 const Home = () => {
   return (
     <div className='wrapper'>
         <h2>Home</h2>
+        <Link to={'/add-thread'} className='new-thread-div'>
+          <p>Add new thread</p>
+          <div className='plus-icon'>+</div>
+        </Link>
         <p>Do we need a home or should the forum be the home page?</p>
     </div>
   )
