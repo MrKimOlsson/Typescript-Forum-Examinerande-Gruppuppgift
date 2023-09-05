2eaffdb834355d8b901e9334fa8fd451e9f7e49c
Kim Olsson <mr.kim.olsson@gmail.com>
2023-09-05 22:36:32 +0200

    commit to mearge

--- src/types.ts
+++ src/types.ts
@@ -4,7 +4,7 @@ export interface User {
 	userName: string;
 }
 
-type ThreadCategory = "THREAD" | "QNA"
+export type ThreadCategory = "THREAD" | "QNA" | "NEWS" | "SPORT" | "POLITICS" | "OTHER" | "GENERAL";
 
 export interface Thread {
   	id: number;
